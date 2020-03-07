02ce2b386b335fdde0220f08bfb8c4d8b5871e3e
Inkant Awasthi <10158214+tim1016@users.noreply.github.com>
2020-03-06 22:29:06 -0600

    Finished data table

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -3,6 +3,7 @@ import { MatTableDataSource } from "@angular/material/table";
 import { Exercise } from "../exercise.model";
 import { ExerciseService } from "../exercise.service";
 import { MatSort } from "@angular/material/sort";
+import { MatPaginator } from "@angular/material/paginator";
 
 @Component({
   selector: "app-past-trainings",
@@ -14,11 +15,18 @@ export class PastTrainingsComponent implements OnInit, AfterViewInit {
   dataSource = new MatTableDataSource<Exercise>();
   constructor(private trainingService: ExerciseService) {}
   @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   ngOnInit(): void {
     this.dataSource.data = this.trainingService.getAllExercises();
   }
 
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  doFilter(filter: string) {
+    this.dataSource.filter = filter.trim().toLowerCase();
   }
 }
